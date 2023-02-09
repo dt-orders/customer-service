62327a8ee7782312d1bbf165571d6a9d5826ca28
robertjahn <rob.jahn@gmail.com>
2023-02-09 13:45:11 -0500

    in buildpush add more versions to push

--- buildpush.sh
+++ buildpush.sh
@@ -19,10 +19,14 @@ echo "Compiling Java"
 docker build -t $IMAGE:1 . --build-arg APP_VERSION=1
 docker build -t $IMAGE:2 . --build-arg APP_VERSION=2
 docker build -t $IMAGE:3 . --build-arg APP_VERSION=3
+docker build -t $IMAGE:4 . --build-arg APP_VERSION=4
+docker build -t $IMAGE:5 . --build-arg APP_VERSION=5
 
 docker tag $IMAGE:1 $IMAGE:1.0.0
 docker tag $IMAGE:2 $IMAGE:2.0.0
 docker tag $IMAGE:3 $IMAGE:3.0.0
+docker tag $IMAGE:3 $IMAGE:4.0.0
+docker tag $IMAGE:3 $IMAGE:5.0.0
 
 echo "========================================================"
 echo "Ready to push images ?"
@@ -40,3 +44,11 @@ docker push $IMAGE:2.0.0
 echo "Pushing $IMAGE:3"
 docker push $IMAGE:3
 docker push $IMAGE:3.0.0
+
+echo "Pushing $IMAGE:4"
+docker push $IMAGE:4
+docker push $IMAGE:4.0.0
+
+echo "Pushing $IMAGE:5"
+docker push $IMAGE:5
+docker push $IMAGE:5.0.0
